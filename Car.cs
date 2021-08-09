ea0ae47ba88f987e1296d6b9d8c5866914257805
umme <unnipa01@yahoo.com>
2021-08-08 23:27:09 -0500

    initial copy

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carMake
+{
+    public class Car
+    { 
+              
+        public string Make {get; set;}
+        public string Made { get; set; }
+        public string Year { get; set; }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace carMake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car thisCar = new Car();
+
+            thisCar.Make = "Toyota";
+            thisCar.Made = "Cambry";
+            thisCar.Year = "2019";
+
+            Console.WriteLine($"The car is {thisCar.Make} {thisCar.Made} {thisCar.Year}");
+
+        }
+    }
+}
